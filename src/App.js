ee2b48a5c323beda8c0390cb01189690af0cb3e9
unknown <duyhung2h@gmail.com>
2023-04-20 12:11:14 +0700

    remove comments & fix sorting by using a work-around

--- src/App.js
+++ src/App.js
@@ -1,27 +1,10 @@
 import React, { useState, useEffect } from "react";
-// import "./styles.css";
 
 import "bootstrap/dist/css/bootstrap.css";
 import "react-bootstrap-table-next/dist/react-bootstrap-table2.min.css";
 
 import DataTable from "react-data-table-component";
 
-// const columns = [
-//   {
-//     dataField: "fullname",
-//     text: "Full Name",
-//     sort: true,
-//   },
-//   {
-//     dataField: "username",
-//     text: "Username",
-//     sort: true,
-//   },
-//   {
-//     dataField: "thumbnail",
-//     text: "Thumbnail Icon",
-//   },
-// ];
 const columns = [
   {
     name: "Full Name",
@@ -49,7 +32,6 @@ export default function App() {
   const [currentPage, setCurrentPage] = useState(1);
   const [perPage, setPerPage] = useState(10);
   const [totalRows, setTotalRows] = useState(0);
-  const [sorting, setSorting] = useState(true);
 
   const fetchData = async (page, per_page) => {
     fetch("https://randomuser.me/api/?page=" + page + "&results=" + per_page)
@@ -78,16 +60,8 @@ export default function App() {
   };
 
   const handlePageChange = (page) => {
-    console.log("old page: " + currentPage);
-    console.log("page changed to: " + page);
-    console.log("sorting: " + sorting);
-    // if (!sorting) {
     setCurrentPage(page);
     fetchData(page, perPage);
-    // alert("handlePageChange");
-    // }else{
-    //   setSorting(false);
-    // }
   };
 
   const handlePerRowsChange = async (newPerPage, page) => {
@@ -100,17 +74,11 @@ export default function App() {
   };
 
   const handleSort = async () => {
-    console.log("sort page: " + currentPage);
-    // setCurrentPage(currentPage);
-    // fetchData(currentPage, perPage);
-    // setSorting(true);
     await sleep(1);
     handlePageChange(currentPage);
-    // alert("handleSort");
   };
 
   useEffect(() => {
-    // alert("useeffect");
     fetchData(currentPage, perPage);
   }, []);
 
@@ -125,7 +93,6 @@ export default function App() {
           columns={columns}
           data={userList}
           pagination
-          // paginationServer
           paginationServer={true}
           paginationTotalRows={totalRows}
           onChangePage={handlePageChange}
